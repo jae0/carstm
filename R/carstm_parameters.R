a5c664ac4c2d27449966b2ebd2eef22aa6d51cbf
Jae S Choi <choi.jae.seok@gmail.com>
2019-06-03 19:29:43 -0300

    update move to bitbucket

--- R/carstm_parameters.R
+++ R/carstm_parameters.R
@@ -8,11 +8,11 @@ carstm_parameters = function( p=NULL, redo=FALSE, ... ) {
   i = which(duplicated(names(p), fromLast = TRUE ))
   if ( length(i) > 0 ) p = p[-i] # give any passed parameters a higher priority, overwriting pre-existing variable
 
-  p = aegis::aegis_parameters( p=p, DS="survey", yrs=p$yrs ) # basic spatial parameters
+  p = aegis.survey::survey_parameters( p=p, yrs=p$yrs ) # basic spatial parameters
 
   # ---------------------
   p$libs = c( p$libs, RLibrary ( "sp", "spdep", "rgeos", "INLA" ))  # standard CRAN libs -- geostatistical support
-  p$libs = c( p$libs, RLibrary( "aegis", "bio.taxonomy", "carstm") ) #,  ) # locally developed code
+  p$libs = c( p$libs, RLibrary( "aegis.ase", "bio.taxonomy", "carstm") ) #,  ) # locally developed code
   # aegis -- support for data layers
   # carstm -- support for stan bym-car models
 
@@ -23,9 +23,9 @@ carstm_parameters = function( p=NULL, redo=FALSE, ... ) {
   if (!exists("trawlable_units", p)) p$trawlable_units = "standardtow"  # for groundfish.db
 
   # set up default map projection
-  p = c(p, aegis::coastline_layout( p=p, redo=redo )
+  p = c(p, aegis.coastline::coastline_layout( p=p, redo=redo ) )
   p$mypalette=RColorBrewer::brewer.pal(9, "YlOrRd")
-  # to plot a map: p = aegis::coastline_layout( p=p, plotmap=TRUE )
+  # to plot a map: p = aegis.coastline::coastline_layout( p=p, plotmap=TRUE )
 
   return(p)
 
