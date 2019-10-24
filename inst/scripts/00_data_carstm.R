0f00de7d60603faf1b4a2150f12745ffefb57bce
Jae S Choi <choi.jae.seok@gmail.com>
2019-10-24 10:18:23 -0300

    doc

---
+++ inst/scripts/00_data_carstm.R
@@ -0,0 +1,2 @@
+# Reminder that this relies upon associated aegis.* data assimilation steps. They need to be completed to the 01.* level.
+# Also, the aegis.survey::01.surveys_data.R is required for higher order indicators.
