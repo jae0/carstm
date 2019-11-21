a519b5290815e9bf8d946adce969f99dce04b58a
Jae S Choi <choi.jae.seok@gmail.com>
2019-11-21 14:23:24 -0400

    add inla group method

--- R/temperature_carstm.R
+++ R/temperature_carstm.R
@@ -78,7 +78,7 @@ temperature_carstm = function ( p=NULL, DS="parameters", redo=FALSE, ... ) {
           inla(
             formula = ', p$variabletomodel, ' ~ 1
               + f( year_factor, model="ar1", hyper=H$ar1 )
-              + f( dyri, model="ar1", scale.model=TRUE, hyper=H$ar1 )
+              + f( dyri, model="ar1", hyper=H$ar1 )
               + f( inla.group( z, method="quantile", n=25 ), model="rw2", scale.model=TRUE, hyper=H$rw2)
               + f( auid, model="bym2", graph=sppoly@nb, group=year_factor, scale.model=TRUE, constr=TRUE, hyper=H$bym2),
             family = "normal",
