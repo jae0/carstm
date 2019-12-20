e93360e5f82ed461af09682f0927f9a0a32f9cba
Jae S Choi <choi.jae.seok@gmail.com>
2019-12-20 08:20:03 -0400

    tweak model

--- R/speciescomposition_carstm.R
+++ R/speciescomposition_carstm.R
@@ -81,7 +81,7 @@ speciescomposition_carstm = function( p=NULL, DS="parameters", redo=FALSE, varna
             + f( inla.group( t, method="quantile", n=13 ), model="rw2", scale.model=TRUE, hyper=H$rw2)
             + f( inla.group( z, method="quantile", n=13 ), model="rw2", scale.model=TRUE, hyper=H$rw2)
             + f( inla.group( substrate.grainsize, method="quantile", n=13 ), model="rw2", scale.model=TRUE, hyper=H$rw2)
-            + f( auid, model="bym2", graph=sppoly@nb, group=year_factor, scale.model=TRUE, constr=TRUE, hyper=H$bym2, control.group=list(model="ar1", hyper=H$ar1)),
+            + f( auid, model="bym2", graph=sppoly@nb, group=year_factor, scale.model=TRUE, constr=TRUE, hyper=H$bym2, control.group=list(model="ar1", hyper=H$ar1_group)),
             family = "normal",
             data= M,
             control.compute = list(dic=TRUE, config=TRUE),
