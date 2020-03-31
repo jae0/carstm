2995702c7af89f4bc333072c108623b72108d421
Jae S Choi <choi.jae.seok@gmail.com>
2020-03-31 13:25:27 -0300

    file naming

---
+++ R/carstm_filenames.R
@@ -0,0 +1,59 @@
+carstm_filenames = function( p,  projecttype="carstm_inputs", projectname=NULL,areal_units_fn=NULL ) {
+
+  if (is.null(areal_units_fn)) {
+    sppoly = areal_units( p=p )  # required by car fit
+    areal_units_fn = attributes(sppoly)[["areal_units_fn"]]
+  }
+
+  if (projecttype=="carstm_inputs") {
+    fnroot = paste(
+      projectname,
+      projecttype,
+      areal_units_fn,
+      p$variabletomodel,
+      p$inputdata_spatial_discretization_planar_km,
+      sep="."
+    )
+
+    if (exists("inputdata_temporal_discretization_yr", p)) {
+      fnroot = paste(
+        fnroot,
+        round(p$inputdata_temporal_discretization_yr, 6),
+        sep="."
+      )
+    }
+    fn = paste( fnroot, "rdata", sep="." )
+    return(fn)
+  }
+
+
+  if (projecttype=="carstm_outputs") {
+
+    if (exists( "inputdata_spatial_discretization_planar_km", p )) {
+      areal_units_fn = paste(
+        areal_units_fn,
+        round(p$inputdata_spatial_discretization_planar_km, 6),
+        sep="_"
+      )
+    }
+
+    if (exists( "inputdata_temporal_discretization_yr", p )) {
+      areal_units_fn = paste(
+        areal_units_fn,
+        round(p$inputdata_temporal_discretization_yr, 6),
+        sep="_"
+      )
+    }
+
+    fn = paste(
+      areal_units_fn,
+      p$variabletomodel,
+      p$carstm_modelengine,
+      "rdata",
+      sep="."
+    )
+
+  }
+
+  return(fn)
+}
