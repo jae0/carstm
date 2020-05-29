f10def00adf1fdb35462860786310852aae90dbd
Jae S Choi <choi.jae.seok@gmail.com>
2020-05-29 09:51:05 -0300

    move aegis functionality to aegis.carstm

--- inst/scripts/00_data_carstm.R
+++
@@ -1,2 +0,0 @@
-# Reminder that this relies upon associated aegis.* data assimilation steps. They need to be completed to the 01.* level.
-# Also, the aegis.survey::01.surveys_data.R is required for higher order indicators.
