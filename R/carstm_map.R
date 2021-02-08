7d7f7c0405a4cce9e948688e88347fb7ebf7e098
Jae S Choi <choi.jae.seok@gmail.com>
2021-02-08 17:51:29 -0400

    carstm cleanup

--- R/carstm_map.R
+++ R/carstm_map.R
@@ -1,5 +1,5 @@
 
-  carstm_map = function( res, vn, poly_match=NULL, time_match=NULL, coastline=NULL, isobaths=NULL,  ...) {
+  carstm_map = function( res, vn, poly_match=NULL, time_match=NULL, coastline=NULL, isobaths=NULL, plot_crs=NULL, ...) {
 
     # carstm/aegis wrapper around spplot, will forward any additional spplot args 
     # TODO move to an sf-based plot
@@ -27,14 +27,24 @@
     }
 
     sppoly[,vn] = toplot
-    
+
+
     ellp = list(...)
 
     if (length(poly_match) > 1 ) {
       dev.new();
+      if (!is.null(plot_crs)) sppoly = st_transform( sppoly, crs=st_crs(plot_crs) )
       plot( sppoly[vn], reset = FALSE, border = "lightslateblue", lwd = 0.8, ...  )
-      if (!is.null(coastline) ) do.call(plot, list(x=coastline, col="whitesmoke", lwd=1.0, add=TRUE) )
-      if (!is.null(isobaths) ) do.call(plot, list(x=isobaths, col="whitesmoke", lwd=0.4, add=TRUE) )
+
+      if (!is.null(coastline) ) {
+        if (!is.null(plot_crs)) coastline = st_transform( coastline, crs=st_crs(plot_crs) )
+        do.call(plot, list(x=coastline, col="whitesmoke", lwd=1.0, add=TRUE) )
+      }
+      
+      if (!is.null(isobaths) ) {
+        if (!is.null(plot_crs)) isobaths = st_transform( isobaths, crs=st_crs(plot_crs) )
+        do.call(plot, list(x=isobaths, col="whitesmoke", lwd=0.4, add=TRUE) )
+      }
     }
   }
 
