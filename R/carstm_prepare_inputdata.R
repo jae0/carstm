caa8b5857d9a4e6dfb2e6542af2634e47122c8df
Jae S Choi <choi.jae.seok@gmail.com>
2021-07-02 06:26:13 -0300

    cleanup lookup

--- R/carstm_prepare_inputdata.R
+++ R/carstm_prepare_inputdata.R
@@ -130,8 +130,7 @@ carstm_prepare_inputdata = function( p, M, sppoly,
           LOCS=M[ iM, c("lon", "lat", "timestamp")],
           lookup_from="core", 
           lookup_to="points", 
-          lookup_from_class="aggregated_data", 
-          variable_name="pca1.mean", 
+          variable_name="pca1", 
           tz="America/Halifax" ,
           year.assessment=p$year.assessment
         )
@@ -147,8 +146,7 @@ carstm_prepare_inputdata = function( p, M, sppoly,
            LOCS=M[ iM, c("lon", "lat", "timestamp")], 
            lookup_from="core", 
            lookup_to="points", 
-           lookup_from_class="aggregated_data", 
-           variable_name="pca2.mean", 
+           variable_name="pca2", 
            tz="America/Halifax" ,
            year.assessment=p$year.assessment
          )
