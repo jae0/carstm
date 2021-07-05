dfdec9de69fc01e5697a33b58df0292d4be5e820
Jae S Choi <choi.jae.seok@gmail.com>
2021-07-05 08:15:45 -0300

    add results unpack

---
+++ R/carstm_results_unpack.R
@@ -0,0 +1,10 @@
+carstm_results_unpack = function( res, vn ) {
+  # to facillitate data extraction dependning upon vn data hierarchy 
+  return( switch( length(vn),
+    res[[ vn[1] ]],
+    res[[ vn[1] ]] [[vn[2] ]],
+    res[[ vn[1] ]] [[vn[2] ]] [[ vn[3] ]],
+    res[[ vn[1] ]] [[vn[2] ]] [[ vn[3] ]] [[vn[4] ]],
+    res[[ vn[1] ]] [[vn[2] ]] [[ vn[3] ]] [[vn[4] ]] [[vn[5] ]]
+  )) 
+}
