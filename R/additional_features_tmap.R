c982fefe11047afe041e8279243020f781a9a51c
Jae S Choi <choijaeseok@gmail.com>
2022-02-25 22:27:28 -0400

    plot tweaks for csas

--- R/additional_features_tmap.R
+++ R/additional_features_tmap.R
@@ -1,5 +1,5 @@
 
-additional_features_tmap = function( p, area_lines=NULL, isobaths=NULL, coastline=NULL ) {
+additional_features_tmap = function( p, area_lines=NULL, isobaths=NULL, coastline=NULL, xlim=c(-80,-40), ylim=c(38, 60) ) {
   require(tmap)
   plot_crs = p$aegis_proj4string_planar_km
 
@@ -12,16 +12,23 @@ additional_features_tmap = function( p, area_lines=NULL, isobaths=NULL, coastlin
   }
 
   if (!is.null(isobaths)) {
-    # isobaths=c( seq(0, 400, by=50), 1000)
+    # isobaths=c(  10, 100, 200, 300, 400, 800 )
     O = O + tm_shape( aegis.bathymetry::isobath_db( depths=isobaths, project_to=plot_crs ), projection=plot_crs ) +
       tm_lines( col="slategray", alpha=0.5, lwd=0.5) 
   }
 
   if (!is.null(coastline)) {
-    # coastline =  c("united states of america", "canada"), xlim=c(-70,-59), ylim=c(40, 50)
-    O = O + tm_shape( st_transform( polygons_rnaturalearth(countries=coastline), st_crs(plot_crs) ), projection=plot_crs ) +
+    # coastline =  c("unsited states of america", "canada")
+    O = O + tm_shape( st_transform( polygons_rnaturalearth(countries=coastline, xlim=xlim, ylim=ylim), st_crs(plot_crs) ), projection=plot_crs ) +
       tm_polygons( col="darkslategray", alpha=0.9, lwd=2)
   }
 
   return(O)
+
+  if (0) {
+    o = additional_features_tmap( p=p, area_lines="cfa.regions", isobaths=c(  10, 100, 200, 300, 400, 800 ), 
+      coastline =  c("unsited states of america", "canada"), xlim=c(-80,-40), ylim=c(38, 60) )
+  }
 }
+
+
