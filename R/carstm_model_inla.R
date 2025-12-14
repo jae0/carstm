51fbf1a5c654fe5ecf30392148406919b89a9074
jae0 <2574393+jae0@users.noreply.github.com>
2025-12-14 08:06:27 -0400

    file name lost error

--- R/carstm_model_inla.R
+++ R/carstm_model_inla.R
@@ -127,6 +127,7 @@ carstm_model_inla = function(
   if (!exists("working.directory", inla_args)) {
     inla_args[["working.directory"]] = tempfile(pattern = "to_delete_", tmpdir = dirname(fn_fit), fileext = "") 
     message("Ensure that the working directory is clear once finished: \n\n", inla_args[["working.directory"]] )
+    wkdir =  inla_args[["working.directory"]] # store for when inla_args gets deleted
   }
 
   # n cores to use for posterior marginals in mcapply .. can be memory intensive so make it a bit less than "num.threads" ..
@@ -1911,11 +1912,11 @@ carstm_model_inla = function(
     message("Summarize results from marginals: ", format(difftime(run_post_samples, run_fit_end)))
     message("Posterior simulations: ", format(difftime(end_post_samples, run_post_samples)))
     message("Total: ", format(difftime(run_end, run_start)))
-    message("Clearing working directory: ", inla_args[["working.directory"]])
+    message("Clearing working directory: ", wkdir )
     message("---------------------------------------")
   } 
 
-  file.remove( inla_args[["working.directory"]] )
+  file.remove( wkdir )
 
   return( fn_fit )   
   
